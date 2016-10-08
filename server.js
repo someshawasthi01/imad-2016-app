5cc03cd47b7513bb7458eaf1f0de09425ff8a663
someshawasthi01 <somesh.awasthi2014@vit.ac.in>
2016-10-08 22:08:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one',function(req,res){
+app.get('/article-one',function(req,res){
    res.send('article one will be served'); 
 });
-app.get('/artice two',function(req,res){
+app.get('/artice-two',function(req,res){
    res.send('article two will be served'); 
 });
-app.get('/article three',function(req,res){
+app.get('/article-three',function(req,res){
    res.send('article three will be served'); 
 });
 app.get('/ui/style.css', function (req, res) {
