e3e7caf582ba892eeb81e7f6d92669cd6e55a86f
someshawasthi01 <somesh.awasthi2014@vit.ac.in>
2016-10-08 22:07:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article one',function(req,res){
    res.send('article one will be served'); 
@@ -18,11 +18,11 @@ app.get('/article three',function(req,res){
    res.send('article three will be served'); 
 });
 app.get('/ui/style.css', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
