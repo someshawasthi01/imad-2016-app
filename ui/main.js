a70e8b94a5ed55c4cf8b38c6ed904d6a0af98414
someshawasthi01 <somesh.awasthi2014@vit.ac.in>
2016-10-09 13:49:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,8 @@
 console.log('Loaded!');
+
+//To change the text of the main-text div
 var element = document.getElementbyId('main-text');
 
 element.innerHTML = 'Fucktards!';
+
+//To move the image on being clicked
