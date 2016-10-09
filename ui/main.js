828f87ec9591cf0770a246f71d53dc58ee39091b
someshawasthi01 <somesh.awasthi2014@vit.ac.in>
2016-10-09 14:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Fucktards!';
 
 //To move the image on being clicked
-var img = document.getElementbyId('madi');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft='100px';
 };
